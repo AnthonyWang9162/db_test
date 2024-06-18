25e28ebadcfea6c538edd98dfbb2b76e17b8ff36
AnthonyWang9162 <anthony9162@gmail.com>
2024-06-18 09:30:01 +0800

    Update test.py

--- test.py
+++ test.py
@@ -2,26 +2,32 @@ import streamlit as st
 import sqlite3
 import os
 from google.oauth2.service_account import Credentials
-from pydrive.auth import GoogleAuth
-from pydrive.drive import GoogleDrive
+from googleapiclient.discovery import build
+from googleapiclient.http import MediaIoBaseDownload, MediaFileUpload
+import io
 
 # 设置 Google Drive API 凭据
 creds = Credentials.from_service_account_info(st.secrets["google_drive"])
 
-# 连接到 Google Drive
-gauth = GoogleAuth()
-gauth.credentials = creds
-drive = GoogleDrive(gauth)
+# 连接到 Google Drive API
+service = build('drive', 'v3', credentials=creds)
 
 # 下载和上传 SQLite 数据库文件的函数
 def download_db(file_id, destination):
-    file = drive.CreateFile({'id': file_id})
-    file.GetContentFile(destination)
+    request = service.files().get_media(fileId=file_id)
+    fh = io.FileIO(destination, 'wb')
+    downloader = MediaIoBaseDownload(fh, request)
+    done = False
+    while done is False:
+        status, done = downloader.next_chunk()
 
 def upload_db(source, file_id):
-    file = drive.CreateFile({'id': file_id})
-    file.SetContentFile(source)
-    file.Upload()
+    file_metadata = {'name': 'test.db'}
+    media = MediaFileUpload(source, mimetype='application/x-sqlite3')
+    updated_file = service.files().update(
+        fileId=file_id,
+        media_body=media
+    ).execute()
 
 # Google Drive 文件 ID（你需要手动获取）
 db_file_id = '1uZ7_ccqZN7UMWwaYzXYYxCRptYsxegFV'
@@ -43,12 +49,12 @@ age = st.text_input('Enter your age')
 
 # 按钮提交数据
 if st.button('Submit'):
-    # 删除数据库中的数据
-    c.execute("DELETE FROM pets WHERE 種類 = ? AND 編號 = ?", (name, age))
+    # 插入数据到数据库
+    c.execute("INSERT INTO pets (種類, 編號) VALUES (?, ?)", (name, age))
     conn.commit()
     # 上传数据库文件到 Google Drive
     upload_db(local_db_path, db_file_id)
-    st.success('Data deleted successfully!')
+    st.success('Data submitted successfully!')
 
 # 显示数据库中的数据
 st.write('Users in database:')
